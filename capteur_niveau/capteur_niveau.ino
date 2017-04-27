1230d5af02cfe21e8649b77227e33c0003041d79
laurentpasek <laurent.pasek@laposte.net>
2017-04-27 19:40:06 +0200

    mise au propre du code.

--- capteur_niveau/capteur_niveau.ino
+++ capteur_niveau/capteur_niveau.ino
@@ -2,7 +2,9 @@
 // letmeknow.fr
 
 int capteur =0;// Le capteur est sur la pin A0
-int val =0;// Variable de stockage de la valeur lue
+float val =0;// Variable de stockage de la valeur lue
+float voltagemap =6;  //valeur lue monvertie en volts
+float voltcalc=6;
 
 void setup ()
 {
@@ -12,6 +14,11 @@ Serial.begin (9600);// Démarrage de la liaison série
 void loop ()
 {
 val = analogRead (capteur);// Lecture de la valeur du capteur
+//voltagemap = map (val, 0, 1023, 0.00, 5.00);  //convertit la valeur en volts et la stocke
+voltcalc = (val * 5) / 1023;
 Serial.println (val);// Affichage de la valeur
-delay (1000);// Attendre 1s avant la prochaine lecture
+//Serial.println (voltagemap);  //affiche le voltage
+Serial.println (voltcalc);
+Serial.println ();
+delay (500);// Attendre 1s avant la prochaine lecture
 }
